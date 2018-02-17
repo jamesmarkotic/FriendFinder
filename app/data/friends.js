abf57f6ddb47ad9c58bd86446779c514c422683a
jamesmarkotic <jamesmarkotic@gmail.com>
2018-02-17 13:43:31 -0800

    Routing is working

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friendArr = [
+  {
+    friendName: 'Derp the Dog',
+    friendPic: 'derp',
+    friendScore: [9001]
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendArr;

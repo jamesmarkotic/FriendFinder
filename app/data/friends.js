bd55329fb98f67326598fe293ac4e706bbf9a902
jamesmarkotic <jamesmarkotic@gmail.com>
2018-02-21 13:16:32 -0800

    Added Links

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,16 @@ var friendArr = [
     friendName: 'Derp the Dog',
     friendPic: 'derp',
     friendScore: [9001]
+  },
+  {
+    friendName: 'Tester Testington',
+    friendPic: 'Test',
+    friendScore: [9002]
+  },
+  {
+    friendName: 'Tester Testington the Second',
+    friendPic: 'Test Jr.',
+    friendScore: [9003]
   }
 ];
 
